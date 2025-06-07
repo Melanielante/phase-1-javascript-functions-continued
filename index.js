211751f3120d56eb05738628e775db53747a9475
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-06-08 00:37:54 +0300

    function mondayWork

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function saturdayFun(activity= "roller-skate") {
 }
 saturdayFun();
 saturdayFun("bathe my dog");
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+mondayWork("work from home.");
